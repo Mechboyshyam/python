7ebc3958af2bc0640e0e7a046299c3713c95354e
shyam <shyamkorade0@gmail.com>
2023-12-31 14:16:40 +0530

    indexing an array

--- practice.py
+++ practice.py
@@ -3,3 +3,12 @@ I have started learning Python Programming.
 I'm loving to learn'''
 
 print(text)
+print("Indexing in array")
+
+name = "Shyam"
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
