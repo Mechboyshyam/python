bdf49620422662dae2588b9f074ff74d69ca6ac4
shyam <shyamkorade0@gmail.com>
2023-12-31 13:54:54 +0530

    calculator program

---
+++ calculator.py
@@ -0,0 +1,21 @@
+# Calculator using python language
+number1 = int(input("Enter the value of a :"))
+number2 = int(input("Enter the value of b :"))
+
+addition = number1 + number2
+print("The addition of ",number1, "+", number2, "is",addition)
+
+substraction = number1 - number2
+print("The addition of ",number1, "-", number2, "is",substraction)
+
+multiplicaton = number1 * number2
+print("The addition of ",number1, "*", number2, "is",multiplicaton)
+
+tothepower = number1 ** number2
+print("The addition of ",number1, "**", number2, "is",tothepower)
+
+division = number1 / number2
+print("The addition of ",number1, "/", number2, "is",division)
+
+floor = number1 // number2
+print("The addition of ",number1, "//", number2, "is",floor)
